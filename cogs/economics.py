a07f61ffc81ac5eefd71ca8eccaaef1afe5f5e79
Clonexy <conexy700@gmail.com>
2022-07-19 15:04:15 +0700

    Fixed setting balance + transfering money command and timely command

--- cogs/economics.py
+++ cogs/economics.py
@@ -1,10 +1,15 @@
+from typing import Optional
+import cooldowns
+
 from nextcord.ext import commands
 from nextcord import Interaction, SlashOption
 import nextcord
-from typing import Optional
+
 from core.money.updaters import update_guild_currency_symbol, update_guild_starting_balance, \
     update_guild_payday_amount, update_user_balance
-from core.money.getters import get_user_balance, get_guild_currency_symbol, get_guild_starting_balance
+from core.money.getters import get_user_balance, get_guild_currency_symbol, get_guild_starting_balance, \
+    get_guild_payday_amount
+from core.money.setters import set_user_balance
 from core.checkers import is_str_or_emoji
 from core.locales.getters import get_msg_from_locale_by_key
 from core.embeds import construct_basic_embed
@@ -103,16 +108,67 @@ class Economics(commands.Cog):
             return await interaction.response.send_message('bot_user_error')
         else:
             starting_balance = get_guild_starting_balance(interaction.guild.id)
-            update_user_balance(interaction.guild.id, user.id, starting_balance)
+            set_user_balance(interaction.guild.id, user.id, starting_balance)
             await interaction.response.send_message('done')
 
     @__reset.subcommand(name="economics", description="Reset's server economics, "
                                                       "all user balances to starting balances")
     async def __economics(self, interaction: Interaction):
         for member in interaction.guild.members:
-            starting_balance = get_guild_starting_balance(interaction.guild.id)
-            update_user_balance(interaction.guild.id, member.id, starting_balance)
+            if not member.bot:
+                starting_balance = get_guild_starting_balance(interaction.guild.id)
+                set_user_balance(interaction.guild.id, member.id, starting_balance)
         await interaction.response.send_message('done')
+
+    @nextcord.slash_command(name="timely", description="Get money per time")
+    @cooldowns.cooldown(1, 7200, bucket=cooldowns.SlashBucket.author)
+    async def __timely(self, interaction: Interaction):
+        payday_amount = get_guild_payday_amount(interaction.guild.id)
+        update_user_balance(interaction.guild.id, interaction.user.id, payday_amount)
+        currency_symbol = get_guild_currency_symbol(interaction.guild.id)
+        message = get_msg_from_locale_by_key(interaction.guild.id, f"{interaction.application_command.name}")
+        requested = get_msg_from_locale_by_key(interaction.guild.id, 'requested_by')
+        await interaction.response.send_message(
+            embed=construct_basic_embed(interaction.application_command.name,
+                                        f"{message}"
+                                        f"+__**{payday_amount}**__ {currency_symbol}",
+                                        f"{requested} {interaction.user}",
+                                        interaction.user.display_avatar))
+
+    @nextcord.slash_command(name="give", description="Transfer your money from balance to other user")
+    async def __give(self, interaction: Interaction, user: Optional[nextcord.Member] = SlashOption(required=True),
+                     money: Optional[int] = SlashOption(required=True)):
+        """
+        Parameters
+        ----------
+        interaction: Interaction
+            The interaction object
+        user: Optional[nextcord.Member]
+            Tag discords member with @
+        money: Optional[int]
+            Number of money you will tranfer to @User
+        """
+        if user.bot:
+            return await interaction.response.send_message('bot_user_error')
+        elif user == interaction.user:
+            return await interaction.response.send_message('self choose error')
+        elif money >= 0 and isinstance(money, int):
+            balance = get_user_balance(interaction.guild.id, interaction.user.id)
+            if balance < money:
+                return await interaction.response.send_message('not_enough_money_error')
+            else:
+                update_user_balance(interaction.guild.id, interaction.user.id, -money)
+                update_user_balance(interaction.guild.id, user.id, money)
+                currency_symbol = get_guild_currency_symbol(interaction.guild.id)
+                message = get_msg_from_locale_by_key(interaction.guild.id, f"{interaction.application_command.name}")
+                requested = get_msg_from_locale_by_key(interaction.guild.id, 'requested_by')
+                await interaction.response.send_message(
+                    embed=construct_basic_embed(interaction.application_command.name,
+                                                f"__**{money}**__ {currency_symbol} {message} {user.mention}\n",
+                                                f"{requested} {interaction.user}",
+                                                interaction.user.display_avatar))
+        else:
+            return await interaction.response.send_message('negative_value_error')
 
 
 def setup(client):
--- core/money/getters.py
+++ core/money/getters.py
@@ -4,8 +4,8 @@ import sqlite3
 def get_user_balance(guild_id: int, user_id: int) -> int:
     db = sqlite3.connect("./databases/main.sqlite")
     cursor = db.cursor()
-    balance = cursor.execute(f"SELECT balance FROM money WHERE guild_id = {guild_id} AND "
-                             f"user_id = {user_id}").fetchone()[0]
+    balance = \
+    cursor.execute(f"SELECT balance FROM money WHERE guild_id = {guild_id} AND user_id = {user_id}").fetchone()[0]
     cursor.close()
     db.close()
     return balance
@@ -29,3 +29,13 @@ def get_guild_starting_balance(guild_id: int) -> int:
     cursor.close()
     db.close()
     return starting_balance
+
+
+def get_guild_payday_amount(guild_id: int) -> int:
+    db = sqlite3.connect("./databases/main.sqlite")
+    cursor = db.cursor()
+    payday_amount = cursor.execute(f"SELECT guild_payday_amount FROM money_config"
+                                   f" WHERE guild_id = {guild_id}").fetchone()[0]
+    cursor.close()
+    db.close()
+    return payday_amount
