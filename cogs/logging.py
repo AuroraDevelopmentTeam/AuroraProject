d4de00a3aadf22b138a53fcc82e2f890064f5544
Clonexy <conexy700@gmail.com>
2022-08-01 13:01:45 +0700

    logging info

---
+++ cogs/logging.py
@@ -0,0 +1,124 @@
+import nextcord
+from nextcord.ext import commands
+
+from core.embeds import construct_log
+
+LOGGING_CHANNEL_TESTING_ID = 999598199444607016
+
+
+class EventsLogging(commands.Cog):
+    def __init__(self, client):
+        self.client = client
+
+    @commands.Cog.listener()
+    async def on_message_delete(self, message):
+        if message.author.bot:
+            return
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Message delete", message.guild, message_channel=message.channel, user=message.author, message=message
+        ))
+
+    @commands.Cog.listener()
+    async def on_message_edit(self, before, after):
+        if after.author.bot:
+            return
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Message edit", after.guild, display_message=False, message=after, jump_url=True,
+            message_channel=after.channel, user=after.author, before=before, after=after
+        ))
+
+    @commands.Cog.listener()
+    async def on_member_join(self, member):
+        if member.bot:
+            return
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Member join", member.guild, user=member
+        ))
+
+    @commands.Cog.listener()
+    async def on_member_remove(self, member):
+        if member.bot:
+            return
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Member remove", member.guild, user=member
+        ))
+
+    @commands.Cog.listener()
+    async def on_member_ban(self, guild, user):
+        if user.bot:
+            return
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Member ban", guild, user=user
+        ))
+
+    @commands.Cog.listener()
+    async def on_member_unban(self, guild, user):
+        if user.bot:
+            return
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Member unban", guild, user=user
+        ))
+
+    @commands.Cog.listener()
+    async def on_guild_role_update(self, before, after):
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Role update", after.guild, role_before=before, role_after=after
+        ))
+
+    @commands.Cog.listener()
+    async def on_guild_role_create(self, role):
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Role create", role.guild, role=role
+        ))
+
+    @commands.Cog.listener()
+    async def on_guild_role_delete(self, role):
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Role delete", role.guild, role=role
+        ))
+
+    @commands.Cog.listener()
+    async def on_guild_channel_create(self, channel):
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Channel create", channel.guild, channel=channel
+        ))
+
+    @commands.Cog.listener()
+    async def on_guild_channel_delete(self, channel):
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Channel create", channel.guild, channel=channel
+        ))
+
+    @commands.Cog.listener()
+    async def on_guild_channel_update(self, before, after):
+        logging_channel = LOGGING_CHANNEL_TESTING_ID
+        logging_channel = self.client.get_channel(logging_channel)
+        await logging_channel.send(embed=construct_log(
+            "Channel create", after.guild, channel_before=before, channel_after=after
+        ))
+
+
+def setup(client):
+    client.add_cog(EventsLogging(client))
--- core/embeds.py
+++ core/embeds.py
@@ -56,3 +56,48 @@ def construct_top_embed(title: str, top_list: list, footer_text: str, footer_url
     embed = nextcord.Embed(color=DEFAULT_BOT_COLOR, title=title, description=description)
     embed.set_footer(text=footer_text, icon_url=footer_url)
     return embed
+
+
+def construct_log(event_type: str, guild: nextcord.Guild, message: nextcord.Message = None, jump_url: bool = False,
+                  display_message: bool = True, message_channel: nextcord.TextChannel = None, user: nextcord.Member =
+                  None, before: nextcord.Message = None, after: nextcord.Message = None, role: nextcord.Role = None,
+                  role_before: nextcord.Role = None, role_after: nextcord.Role = None,
+                  channel=None, channel_before=None, channel_after=None) -> nextcord.Embed:
+    embed = nextcord.Embed(color=DEFAULT_BOT_COLOR, title=event_type)
+    if message_channel is not None:
+        embed.add_field(name="Channel", value=f"{channel.mention}\nID: **{channel.id}**", inline=False)
+    if user is not None:
+        embed.add_field(name="User", value=f"{user.mention}\n`{user}`, ID: **{user.id}**", inline=False)
+        embed.set_thumbnail(url=f"{user.display_avatar}")
+    if jump_url is True:
+        embed.add_field(name="URL", value=f"[message]({message.jump_url})", inline=False)
+    if message is not None and display_message is True:
+        embed.add_field(name="Message", value=f"``{message.content}``", inline=False)
+    if before is not None:
+        embed.add_field(name="Before", value=f"``{before.content}``", inline=True)
+    if after is not None:
+        embed.add_field(name="After", value=f"``{after.content}``", inline=True)
+    if role is not None:
+        embed.add_field(name="Role", value=f"{role.mention}\n"
+                                           f"ID: **{role.id}**, name: ``{role.name}``\n"
+                                           f"Position: **{role.position}**, mentionable: **{role.mentionable}**\n"
+                                           f"Permissions: **{role.permissions.value}**", inline=False)
+    if role_before is not None:
+        embed.add_field(name="Before", value=f"``@{role_before.name}``, ``{role_before.color}``\n"
+                                             f"Position: **{role_before.position}**, mentionable: "
+                                             f"**{role_before.mentionable}**\n"
+                                             f"Permissions: **{role_before.permissions.value}**", inline=True)
+    if role_after is not None:
+        embed.add_field(name="After", value=f"{role_after.mention}\n``@{role_after.name}``, ``{role_after.color}``\n"
+                                            f"Position: **{role_after.position}**, mentionable: "
+                                            f"**{role_after.mentionable}**\n"
+                                            f"Permissions: **{role_after.permissions.value}**", inline=True)
+    if channel is not None:
+        embed.add_field(name="Channel", value=f"``{channel.name}``, {channel.mention}", inline=False)
+    if channel_before is not None:
+        embed.add_field(name="Before", value=f"``{channel_before.name}``", inline=True)
+    if channel_after is not None:
+        embed.add_field(name="After", value=f"``{channel_after.name}``, {channel_after.mention}\n"
+                                            f"Changed roles: {channel_after.changed_roles} ", inline=True)
+    embed.set_footer(text=f"{guild.name}\nAURORA", icon_url=guild.icon)
+    return embed
