96fa6804137ecd56421d9fb36cec36d178b35671
Clonexy <conexy700@gmail.com>
2022-08-07 23:39:02 +0700

    tickets testing setup

--- cogs/tickets.py
+++ cogs/tickets.py
@@ -6,6 +6,20 @@ from core.money.getters import get_guild_currency_symbol
 from core.locales.getters import get_msg_from_locale_by_key
 
 
+class TicketDisabled(nextcord.ui.View):
+    def __init__(self):
+        super().__init__(timeout=None)
+
+    @nextcord.ui.button(
+        label="Close ticket",
+        style=nextcord.ButtonStyle.red,
+        custom_id="ticket_settings:red",
+        disabled=True
+    )
+    async def close_ticket(self, button: nextcord.ui.Button, interaction: Interaction):
+        pass
+
+
 class TicketSettings(nextcord.ui.View):
     def __init__(self):
         super().__init__(timeout=None)
@@ -14,6 +28,7 @@ class TicketSettings(nextcord.ui.View):
         label="Close ticket",
         style=nextcord.ButtonStyle.red,
         custom_id="ticket_settings:red",
+        disabled=False
     )
     async def close_ticket(self, button: nextcord.ui.Button, interaction: Interaction):
         pass
@@ -48,21 +63,27 @@ class Tickets(commands.Cog):
         try:
             custom_id = interaction.data["custom_id"]
             if custom_id == "create_ticket:blurple":
+                await interaction.response.defer()
                 embed = nextcord.Embed(
                     title="Ticket", description=f"Your ticket is creating..."
                 )
-                msg = await interaction.response.send_message(
+                msg = await interaction.followup.send(
                     embed=embed, ephemeral=True
                 )
+                support_role = nextcord.utils.get(interaction.guild.roles, name="ticket_support")
                 overwrites = {
                     interaction.guild.default_role: nextcord.PermissionOverwrite(
                         read_messages=False
                     ),
-                    interaction.guild.me: nextcord.PermissionOverwrite(
+                    interaction.user: nextcord.PermissionOverwrite(
                         read_messages=True
                     ),
+                    support_role: nextcord.PermissionOverwrite(
+                        read_messages=True
+                    )
                 }
-                channel = await interaction.guild.create_text_channel(
+                category_channel = nextcord.utils.get(interaction.guild.categories, name="Aurora-Tickets")
+                channel = await category_channel.create_text_channel(
                     f"{interaction.user.name}-ticket", overwrites=overwrites
                 )
                 embed = nextcord.Embed(
@@ -77,8 +98,16 @@ class Tickets(commands.Cog):
                 await channel.send(embed=embed, view=TicketSettings())
             if custom_id == "ticket_settings:red":
                 await interaction.channel.send("Closing this ticket...")
-                await interaction.channel.delete()
+                category_channel = nextcord.utils.get(interaction.guild.categories, name="Aurora-Archive")
+                overwrites = {
+                    interaction.guild.default_role: nextcord.PermissionOverwrite(
+                        read_messages=False
+                    )
+                }
                 await interaction.user.send(f"Ticket closed!")
+                await interaction.message.edit(view=TicketDisabled())
+                await interaction.channel.send(f"Ticket closed!")
+                await interaction.channel.edit(category=category_channel, overwrites=overwrites, sync_permissions=True)
         except KeyError:
             pass
 
@@ -90,8 +119,18 @@ class Tickets(commands.Cog):
         embed = nextcord.Embed(
             title="Create a ticket",
             description="Click `create ticket` button below to create a ticket. "
-            "The server's staff will be notified and solve your problem.",
+                        "The server's staff will be notified and solve your problem.",
         )
+        overwrites = {
+            interaction.guild.default_role: nextcord.PermissionOverwrite(
+                read_messages=False
+            ),
+            interaction.guild.me: nextcord.PermissionOverwrite(
+                read_messages=True
+            ),
+        }
+        tickets_category = await interaction.guild.create_category(name=f"Aurora-Tickets", overwrites=overwrites)
+        archive_category = await interaction.guild.create_category(name="Aurora-Archive", overwrites=overwrites)
         await interaction.response.send_message(embed=embed, view=CreateTicket())
 
 
