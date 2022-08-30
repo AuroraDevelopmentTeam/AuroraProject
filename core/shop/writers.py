4506abbefacb4e7253cd2cd3fc17428fcbedd5cd
Z10kk <87130324+10kkm@users.noreply.github.com>
2022-08-30 19:53:59 +0300

    removed useless code

--- core/shop/writers.py
+++ core/shop/writers.py
@@ -39,11 +39,3 @@ def write_role_in_custom_shop(
     db.close()
 
 
-def delete_role_from_custom_shop(guild_id: int, role: nextcord.Role) -> None:
-    db = sqlite3.connect("./databases/main.sqlite")
-    cursor = db.cursor()
-    sql = f"DELETE FROM custom_shop WHERE role_id = {role.id} AND guild_id = {guild_id}"
-    cursor.execute(sql)
-    db.commit()
-    cursor.close()
-    db.close()
