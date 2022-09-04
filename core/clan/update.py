c9c3f1bc2537ecdcd968459a98511619eba22562
Clonexy <conexy700@gmail.com>
2022-09-04 17:35:20 +0700

    Клановая система, с поддержкой перевода языка на клиентский, всеми проверками и полной работоспоспобностью

--- core/clan/update.py
+++ core/clan/update.py
@@ -1,6 +1,8 @@
 import random
 import sqlite3
 
+import nextcord
+
 from core.clan.getters import (
     get_clan_max_attack,
     get_clan_min_attack,
@@ -10,7 +12,20 @@ from core.clan.getters import (
     get_clan_guild_boss_hp,
     get_clan_guild_boss_level,
     get_clan_boss_hp_limit,
+    get_server_clan_change_image_cost,
+    get_server_clan_upgrade_boss_cost,
+    get_server_clan_upgrade_attack_cost,
+    get_server_clan_change_icon_cost,
+    get_server_clan_upgrade_limit_cost,
+    get_server_clan_create_cost,
+    get_upgrade_limit_multiplier,
+    get_boss_upgrade_multiplier,
+    get_clan_member_limit,
+    get_user_clan_id
 )
+from core.locales.getters import get_msg_from_locale_by_key, localize_name
+from core.emojify import SHOP
+from core.embeds import DEFAULT_BOT_COLOR
 
 from core.clan.storage import boss_rewards
 
@@ -217,6 +232,18 @@ def update_clan_icon_on_creation(guild_id: int, owner_id: int, icon: str) -> Non
     return
 
 
+def update_clan_member_limit(guild_id: int, clan_id: int, new_member_limit: int) -> None:
+    db = sqlite3.connect("./databases/main.sqlite")
+    cursor = db.cursor()
+    sql = "UPDATE clans SET member_limit = ? WHERE guild_id = ? AND clan_id = ?"
+    values = (new_member_limit, guild_id, clan_id)
+    cursor.execute(sql, values)
+    db.commit()
+    cursor.close()
+    db.close()
+    return
+
+
 def update_user_clan_id(guild_id: int, user_id: int, clan_id: int) -> None:
     db = sqlite3.connect("./databases/main.sqlite")
     cursor = db.cursor()
@@ -397,3 +424,65 @@ def calculate_level(guild_id: int, clan_id: int):
             leveling_formula = round((17 * (clan_level ** 3)) + 11)
 
 
+def redraw_shop_embed(interaction: nextcord.Interaction) -> nextcord.Embed:
+    embed = nextcord.Embed(
+        color=DEFAULT_BOT_COLOR,
+        title=f"{SHOP} {localize_name(interaction.guild.id, 'clan_shop').capitalize()}",
+    )
+    embed.add_field(name="```#.  ```", value="```1. ```", inline=True)
+    embed.add_field(
+        name="```                 Имя Товара                 ```",
+        value=f"```{get_msg_from_locale_by_key(interaction.guild.id, 'clan_shop_1')}```",
+        inline=True,
+    )
+    embed.add_field(
+        name=f"```   {get_msg_from_locale_by_key(interaction.guild.id, 'price')}   ```",
+        value=f"``` {get_server_clan_upgrade_limit_cost(interaction.guild.id) * get_upgrade_limit_multiplier(get_clan_member_limit(interaction.guild.id, get_user_clan_id(interaction.guild.id, interaction.user.id)))}```",
+        inline=True,
+    )
+    embed.add_field(name="```#.  ```", value="```2. ```", inline=True)
+    embed.add_field(
+        name="```                 Имя Товара                 ```",
+        value=f"```{get_msg_from_locale_by_key(interaction.guild.id, 'clan_shop_2')}```",
+        inline=True,
+    )
+    embed.add_field(
+        name=f"```   {get_msg_from_locale_by_key(interaction.guild.id, 'price')}   ```",
+        value=f"``` {get_server_clan_change_image_cost(interaction.guild.id)}```",
+        inline=True,
+    )
+    embed.add_field(name="```#.  ```", value="```3. ```", inline=True)
+    embed.add_field(
+        name="```                 Имя Товара                 ```",
+        value=f"```{get_msg_from_locale_by_key(interaction.guild.id, 'clan_shop_3')}```",
+        inline=True,
+    )
+    embed.add_field(
+        name=f"```   {get_msg_from_locale_by_key(interaction.guild.id, 'price')}   ```",
+        value=f"``` {get_server_clan_change_icon_cost(interaction.guild.id)}```",
+        inline=True,
+    )
+    embed.add_field(name="```#.  ```", value="```4. ```", inline=True)
+    embed.add_field(
+        name="```                 Имя Товара                 ```",
+        value=f"```{get_msg_from_locale_by_key(interaction.guild.id, 'clan_shop_4')}```",
+        inline=True,
+    )
+    embed.add_field(
+        name=f"```   {get_msg_from_locale_by_key(interaction.guild.id, 'price')}   ```",
+        value=f"``` {get_server_clan_upgrade_attack_cost(interaction.guild.id)}```",
+        inline=True,
+    )
+    embed.add_field(name="```#.  ```", value="```5. ```", inline=True)
+    embed.add_field(
+        name="```                 Имя Товара                 ```",
+        value=f"```{get_msg_from_locale_by_key(interaction.guild.id, 'clan_shop_5')}```",
+        inline=True,
+    )
+    embed.add_field(
+        name=f"```   {get_msg_from_locale_by_key(interaction.guild.id, 'price')}   ```",
+        value=f"``` "
+              f"{get_server_clan_upgrade_boss_cost(interaction.guild.id) * get_boss_upgrade_multiplier(get_clan_guild_boss_level(interaction.guild.id, get_user_clan_id(interaction.guild.id, interaction.user.id)))}```",
+        inline=True,
+    )
+    return embed
